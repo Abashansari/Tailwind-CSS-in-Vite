23cb4cb4448ead29306e0a83ef57a964b4dab31a
Abash Ansari <ansariabash2004@gmail.com>
2025-09-14 19:53:09 +0530

    created a simple navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,18 @@ function App() {
 
   return (
     <>
-  <h1 className="text-3xl font-bold underline bg-red-600">Hello world!</h1>
+    <nav className='container border-2 border-t-4 border-black my-4 flex justify-between items-center text-purple-500 mx-auto px-6'>
+      <div className=" text-3xl">Portfolio</div>
+      <ul className='flex space-x-3 my-4 '>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    <div className='container bg-yellow-500 sm:bg-green-500 md:bg-blue-400 lg:bg-red-600'>
+      <p>Hello from abash</p>
+    </div>
+
     </>
   )
 }
