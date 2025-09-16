28fd05dc0b30473dfbe00efd961184c3a3f31c5e
Abash Ansari <ansariabash2004@gmail.com>
2025-09-16 19:32:23 +0530

    created a 2 columns using grid utilities

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-        <div className='container bg-yellow-500 sm:bg-green-500 md:bg-blue-400 lg:bg-red-600'>
-      <p>Hello from abash</p>
+<div className='container'>
+    <div className="grid grid-cols-2 gap-5 h-180 justify-between mx-30 ">
+    <div className='container flex justify-center border-2 bg-blue-500 '>
+    <h1 className='text-2xl p-[30%]'>Left Side</h1>
+    </div>
+    <div className="container flex justify-center border-2 bg-blue-500">
+      <h1 className='text-2xl p-[30%]'>Right side</h1>
     </div>
     </div>
+
+</div>
   )
 }
